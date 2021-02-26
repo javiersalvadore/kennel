fa2276c210c7eaf10e0b2b64035b55de1d0e5295
unknown <javiersalvadore@gmail.com>
2021-02-26 10:17:24 -0300

    testing js

---
+++ resources/js/script.js
@@ -0,0 +1,12 @@
+let images = document.getElementsByClassName('img');
+const overlay = (element) => {
+    let paragraph = document.createElement('p');
+    paragraph.innerHTML = `Nombre: ${element.h5.innerHTML}<br> Fecha de nacimiento: 17/12/2020 <br> Padres: Amazonas y Leopoldo`;
+    element.img.style.blur = '8px';
+    paragraph.style.fontSize = '29px';
+    paragraph.style.textAlign = 'center';
+    element.appendChild(paragraph);
+};
+for (let i = 0; i < image.length; i++) {
+    images[i].addEventListener('hover', overlay(images[i]));
+};
