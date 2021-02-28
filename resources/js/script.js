d469b419a3cb7d5da52b5b40e7ff65ae29cbbdd0
unknown <javiersalvadore@gmail.com>
2021-02-28 11:26:47 -0300

    tampering with js script

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,12 +1,13 @@
-let images = document.getElementsByTagName('img');
+let images = document.getElementsByTagName("img");
 const overlay = (element) => {
-    let paragraph = document.createElement('p');
+    /*let paragraph = document.createElement('p');
     paragraph.innerHTML = `Nombre: ${element.h5.innerHTML}<br> Fecha de nacimiento: 17/12/2020 <br> Padres: Amazonas y Leopoldo`;
-    element.img.style.blur = '8px';
+    
     paragraph.style.fontSize = '29px';
     paragraph.style.textAlign = 'center';
-    element.appendChild(paragraph);
+    element.appendChild(paragraph);*/
+    element.style.blur = '8px';
 };
-for (let i = 0; i < image.length; i++) {
-    images[i].onmouseover(overlay(images[i]));
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener('mouseover',overlay(images[i]));
 };
