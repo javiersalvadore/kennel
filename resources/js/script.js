0a6e959efe568b3831f447e679b9c9ebbe18503d
unknown <javiersalvadore@gmail.com>
2021-02-28 12:00:08 -0300

    minimal work

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,15 +1,1 @@
 let images = document.getElementsByTagName("img");
-const overlay = (element) => {
-    /*let paragraph = document.createElement('p');
-    paragraph.innerHTML = `Nombre: ${element.h5.innerHTML}<br> Fecha de nacimiento: 17/12/2020 <br> Padres: Amazonas y Leopoldo`;
-    
-    paragraph.style.fontSize = '29px';
-    paragraph.style.textAlign = 'center';
-    element.appendChild(paragraph);*/
-    element.style.blur = '8px';
-};
-for (let i = 0; i < images.length; i++) {
-    images[i].onmouseover = overlay(images[i]);
-    console.log(i);
-};
-console.log(images)
