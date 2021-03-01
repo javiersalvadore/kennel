9f225aade07aa17082e5f9e5a991ac63998b05cd
unknown <javiersalvadore@gmail.com>
2021-03-01 18:18:40 -0300

    unsure what I did

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,14 @@
 let images = document.getElementsByTagName("img");
+
+const enterBlur = (element) => {
+    element.style.animation = 'blurIn 1s 1';
+}
+const exitBlur = (element) => {
+    element.style.animation = 'blurOut 1s 1';
+}
+
+for (let i = 0; i < images.length; i++) {
+    images[i].onmouseenter = enterBlur(images[i]);
+    images[i].onmouseleave = exitBlur(images[i]);
+}
+console.log(images[1])
