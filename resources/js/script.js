846f06f80c5e3e81da74876ed17d481a5992d0e3
oledtrix <javiersalvadore@gmail.com>
2021-02-28 22:43:34 -0300

    experimenting

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,11 @@
 let images = document.getElementsByTagName("img");
+function BlurElements(element) {
+    element.style.animation = 'blurIn 1s';
+};
+function unBlurElements(element) {
+    element.style.animation = 'blurOut 1s'
+};
+for (let elem = 0; elem < images.length; elem++) {
+    images[elem].onmouseenter = BlurElements(this);
+    images[elem].onmouse = BlurElements(this);
+};
