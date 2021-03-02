25d98b2ae11fe3907e9112abd82c35806b25c530
oledtrix <javiersalvadore@gmail.com>
2021-03-02 19:27:04 -0300

    reset js

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,7 +5,3 @@ function unBlurElements(element) {
     element.style.animation = 'blurOut 1s';
 };
 let images = document.getElementsByTagName('img');
-console.log(images.length);
-let picture = document.getElementById('pic1');
-picture.addEventListener('mouseenter', BlurElements(picture), false);
-picture.addEventListener('mouseleave', unBlurElements(picture), false);
