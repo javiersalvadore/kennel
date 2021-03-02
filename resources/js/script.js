e14e8fb575dee8b4e13aab6a16a5cd81599f36e5
unknown <javiersalvadore@gmail.com>
2021-03-02 19:20:42 -0300

    testing js again

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,11 +1,11 @@
-let images = document.getElementsByTagName("img");
 function BlurElements(element) {
     element.style.animation = 'blurIn 1s';
 };
 function unBlurElements(element) {
-    element.style.animation = 'blurOut 1s'
+    element.style.animation = 'blurOut 1s';
 };
-for (let elem = 0; elem < images.length; elem++) {
-    images[elem].onmouseenter = BlurElements(this);
-    images[elem].onmouse = BlurElements(this);
-};
+let images = document.getElementsByTagName('img');
+console.log(images.length);
+let picture = document.getElementById('pic1');
+picture.addEventListener('mouseenter', BlurElements(picture), false);
+picture.addEventListener('mouseleave', unBlurElements(picture), false);
