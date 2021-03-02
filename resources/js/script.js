d2be7faf96093a46be1d0ba8d88774660cf90f47
oledtrix <javiersalvadore@gmail.com>
2021-03-02 20:29:35 -0300

    js finally working

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,3 +5,7 @@ function unBlurElements(element) {
     element.style.animation = 'blurOut 1s';
 };
 let images = document.getElementsByTagName('img');
+for(let i = 0; i < images.length; i++) {
+    images[i].addEventListener('mouseover', () => {BlurElements(images[i])});
+    images[i].addEventListener('mouseleave', () => {unBlurElements(images[i])});
+};
